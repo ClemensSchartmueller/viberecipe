bd8063f056507ece9088ec860ea08bfaf3973053
Clemens Schartmüller <46600871+ClemensSchartmueller@users.noreply.github.com>
2025-12-15 16:02:41 +0100

    Fix static folder deployment?

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  // Ensure we don't have issues with strict mode matching dev behavior
+  reactStrictMode: true,
 };
 
 export default nextConfig;
